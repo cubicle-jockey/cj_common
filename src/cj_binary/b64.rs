7cd3c7422ad42f2ed5af8ac990f29a009e3b19c7
cubicle-jockey <110308650+cubicle-jockey@users.noreply.github.com>
2022-08-07 01:24:48 -0500

    added base64_to_bytes method that decodes a str to vec of u8.
    added an iterator that decodes to u8 when iterating over a base64 encoded str.

--- src/cj_binary/b64.rs
+++ src/cj_binary/b64.rs
@@ -1,5 +1,8 @@
 pub mod b64 {
     use std::slice::Iter;
+    use std::str::Chars;
+
+    use crate::prelude::CharToOrdResult::{Invalid, Pad, WhiteSpace};
 
     // general b64 table (RFC 4648.4).
     static B64_TABLE: [char; 64] = [
@@ -8,6 +11,111 @@ pub mod b64 {
         'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1',
         '2', '3', '4', '5', '6', '7', '8', '9', '+', '/',
     ];
+
+    #[derive(Debug, PartialOrd, PartialEq, Ord, Eq)]
+    pub enum CharToOrdResult {
+        Ok(u8),
+        Pad,
+        WhiteSpace,
+        Invalid,
+    }
+
+    impl CharToOrdResult {
+        #[inline]
+        pub fn is_ok(&self) -> bool {
+            *self < CharToOrdResult::Pad
+        }
+        #[inline]
+        pub fn is_padding(&self) -> bool {
+            *self == CharToOrdResult::Pad
+        }
+        #[inline]
+        pub fn is_whitespace(&self) -> bool {
+            *self == CharToOrdResult::WhiteSpace
+        }
+        #[inline]
+        pub fn is_invalid(&self) -> bool {
+            *self == CharToOrdResult::Invalid
+        }
+    }
+
+    #[inline]
+    pub fn b64_char_to_ord(c: &char) -> &'static CharToOrdResult {
+        match c {
+            'A' => &CharToOrdResult::Ok(0),
+            'B' => &CharToOrdResult::Ok(1),
+            'C' => &CharToOrdResult::Ok(2),
+            'D' => &CharToOrdResult::Ok(3),
+            'E' => &CharToOrdResult::Ok(4),
+            'F' => &CharToOrdResult::Ok(5),
+            'G' => &CharToOrdResult::Ok(6),
+            'H' => &CharToOrdResult::Ok(7),
+            'I' => &CharToOrdResult::Ok(8),
+            'J' => &CharToOrdResult::Ok(9),
+            'K' => &CharToOrdResult::Ok(10),
+            'L' => &CharToOrdResult::Ok(11),
+            'M' => &CharToOrdResult::Ok(12),
+            'N' => &CharToOrdResult::Ok(13),
+            'O' => &CharToOrdResult::Ok(14),
+            'P' => &CharToOrdResult::Ok(15),
+            'Q' => &CharToOrdResult::Ok(16),
+            'R' => &CharToOrdResult::Ok(17),
+            'S' => &CharToOrdResult::Ok(18),
+            'T' => &CharToOrdResult::Ok(19),
+            'U' => &CharToOrdResult::Ok(20),
+            'V' => &CharToOrdResult::Ok(21),
+            'W' => &CharToOrdResult::Ok(22),
+            'X' => &CharToOrdResult::Ok(23),
+            'Y' => &CharToOrdResult::Ok(24),
+            'Z' => &CharToOrdResult::Ok(25),
+            'a' => &CharToOrdResult::Ok(26),
+            'b' => &CharToOrdResult::Ok(27),
+            'c' => &CharToOrdResult::Ok(28),
+            'd' => &CharToOrdResult::Ok(29),
+            'e' => &CharToOrdResult::Ok(30),
+            'f' => &CharToOrdResult::Ok(31),
+            'g' => &CharToOrdResult::Ok(32),
+            'h' => &CharToOrdResult::Ok(33),
+            'i' => &CharToOrdResult::Ok(34),
+            'j' => &CharToOrdResult::Ok(35),
+            'k' => &CharToOrdResult::Ok(36),
+            'l' => &CharToOrdResult::Ok(37),
+            'm' => &CharToOrdResult::Ok(38),
+            'n' => &CharToOrdResult::Ok(39),
+            'o' => &CharToOrdResult::Ok(40),
+            'p' => &CharToOrdResult::Ok(41),
+            'q' => &CharToOrdResult::Ok(42),
+            'r' => &CharToOrdResult::Ok(43),
+            's' => &CharToOrdResult::Ok(44),
+            't' => &CharToOrdResult::Ok(45),
+            'u' => &CharToOrdResult::Ok(46),
+            'v' => &CharToOrdResult::Ok(47),
+            'w' => &CharToOrdResult::Ok(48),
+            'x' => &CharToOrdResult::Ok(49),
+            'y' => &CharToOrdResult::Ok(50),
+            'z' => &CharToOrdResult::Ok(51),
+            '0' => &CharToOrdResult::Ok(52),
+            '1' => &CharToOrdResult::Ok(53),
+            '2' => &CharToOrdResult::Ok(54),
+            '3' => &CharToOrdResult::Ok(55),
+            '4' => &CharToOrdResult::Ok(56),
+            '5' => &CharToOrdResult::Ok(57),
+            '6' => &CharToOrdResult::Ok(58),
+            '7' => &CharToOrdResult::Ok(59),
+            '8' => &CharToOrdResult::Ok(60),
+            '9' => &CharToOrdResult::Ok(61),
+            '+' => &CharToOrdResult::Ok(62),
+            '/' => &CharToOrdResult::Ok(63),
+            '=' => &CharToOrdResult::Pad,
+            // chars to ignore
+            ' ' => &CharToOrdResult::WhiteSpace,
+            '\r' => &CharToOrdResult::WhiteSpace,
+            '\n' => &CharToOrdResult::WhiteSpace,
+            '\t' => &CharToOrdResult::WhiteSpace,
+            // all else are failures
+            _ => &CharToOrdResult::Invalid,
+        }
+    }
 
     // fn bytes_to_b64(bytes: &[u8]) -> String {
     //     let mut s = String::with_capacity((bytes.len() as f64 * 1.25) as usize + 2);
@@ -210,7 +318,7 @@ pub mod b64 {
     /// Iterator for a slice of bytes that returns Base64
     ///
     /// ```
-    ///  # use cj_common::prelude::CjBase64Iter;
+    ///  # use cj_common::prelude::CjToBase64Iter;
     ///  # use crate::cj_common::cj_binary::b64::b64::ToBase64Iter;
     /// // slice of bytes example
     /// let s = "Many hands make light work.".as_bytes();
@@ -259,14 +367,20 @@ pub mod b64 {
             } else {
                 if let Some(b) = &self.inner.next() {
                     let c: char;
-                    match bit_split_6(b, &mut self.six_bits, &mut self.rem_bits, &mut self.pass_no) {
+                    match bit_split_6(b, &mut self.six_bits, &mut self.rem_bits, &mut self.pass_no)
+                    {
                         BitSplit6Result::Ready => {
                             c = B64_TABLE[self.six_bits as usize];
                             self.char_count += 1;
                         }
                         BitSplit6Result::Resend => {
                             c = B64_TABLE[self.six_bits as usize];
-                            let _ = bit_split_6(b, &mut self.six_bits, &mut self.rem_bits, &mut self.pass_no);
+                            let _ = bit_split_6(
+                                b,
+                                &mut self.six_bits,
+                                &mut self.rem_bits,
+                                &mut self.pass_no,
+                            );
                             self.pend_char = Some(B64_TABLE[self.six_bits as usize]);
                             self.char_count += 2;
                         }
@@ -277,7 +391,12 @@ pub mod b64 {
                         let x = self.rem_bits.clone();
                         self.rem_bits = 0;
                         self.pass_no = 0;
-                        let _ = bit_split_6(&x, &mut self.six_bits, &mut self.rem_bits, &mut self.pass_no);
+                        let _ = bit_split_6(
+                            &x,
+                            &mut self.six_bits,
+                            &mut self.rem_bits,
+                            &mut self.pass_no,
+                        );
                         self.char_count += 1;
                         self.push_last = false;
                         return Some(B64_TABLE[self.six_bits as usize]);
@@ -299,20 +418,237 @@ pub mod b64 {
         }
     }
 
-    pub trait CjBase64Iter {
+    pub trait CjToBase64Iter {
         fn iter_base64(&self) -> ToBase64Iter;
     }
 
-    impl CjBase64Iter for &[u8] {
+    impl CjToBase64Iter for &[u8] {
         fn iter_base64(&self) -> ToBase64Iter {
             ToBase64Iter::new(self[..].iter())
         }
     }
 
-    impl CjBase64Iter for Vec<u8> {
+    impl CjToBase64Iter for Vec<u8> {
         fn iter_base64(&self) -> ToBase64Iter {
             ToBase64Iter::new(self[..].iter())
         }
+    }
+
+    ///
+    /// Iterator for a Base64 encoded str that returns byte
+    ///
+    /// ```
+    ///  # use cj_common::prelude::CjFromBase64Iter;
+    ///  # use crate::cj_common::cj_binary::b64::b64::FromBase64Iter;
+    /// // slice of bytes example
+    /// let mut v = Vec::new();
+    /// for b in "TWFueSBoYW5kcyBtYWtlIGxpZ2h0IHdvcmsu".iter_b64_to_byte() {
+    ///     v.push(b);
+    /// }
+    /// assert!(v.len() > 0);
+    /// let r = String::from_utf8_lossy(v.as_slice());
+    /// let s = "Many hands make light work.";
+    /// assert_eq!(r.to_string().as_str(), s);
+    /// ```
+    pub struct FromBase64Iter<'a> {
+        buf_left: u8,
+        buf_ct: u8,
+        had_pad: bool,
+        total_added: usize,
+        inner: Chars<'a>,
+    }
+
+    impl<'a> FromBase64Iter<'a> {
+        pub fn new(i: Chars<'a>) -> Self {
+            Self {
+                buf_left: 0,
+                buf_ct: 0,
+                had_pad: false,
+                total_added: 0,
+                inner: i,
+            }
+        }
+
+        fn next_byte(&mut self) -> Option<u8> {
+            let return_b: u8;
+            while let Some(c) = &self.inner.next() {
+                let r = b64_char_to_ord(c);
+                match r {
+                    CharToOrdResult::Ok(b) => {
+                        if self.had_pad {
+                            break;
+                        }
+                        match self.buf_ct {
+                            0 => {
+                                // buf_left is empty
+                                // drop two bits from 'b' and take 6
+                                // 00XXXXXX
+                                self.buf_left = b << 2;
+                                self.buf_ct += 1;
+                            }
+                            1 => {
+                                // buf_left has 6 bits XXXXXX00
+                                // drop two bits from 'b' and take 2
+                                self.buf_left += (b << 2) >> 6;
+                                // push the complete byte
+                                return_b = self.buf_left.clone();
+                                // now take the remaining 4
+                                self.buf_left = b << 4;
+                                self.buf_ct += 1;
+                                self.total_added += 1;
+                                return Some(return_b);
+                            }
+                            2 => {
+                                // buf_left has 4 bits XXXX0000
+                                // drop two bits from 'b' and take 4
+                                self.buf_left += (b << 2) >> 4;
+                                // push the complete byte
+                                return_b = self.buf_left.clone();
+                                self.buf_left = b << 6;
+                                self.buf_ct += 1;
+                                self.total_added += 1;
+                                return Some(return_b);
+                            }
+                            3 => {
+                                // buf_left has 2 bits XX000000
+                                // 'b' only has 6 bits. take them all
+                                self.buf_left += b;
+                                // push the complete byte
+                                return_b = self.buf_left.clone();
+                                self.buf_ct = 0;
+                                self.total_added += 1;
+                                return Some(return_b);
+                            }
+                            _ => unreachable!(),
+                        }
+                    }
+                    Pad => self.had_pad = true,
+                    WhiteSpace => {
+                        if self.had_pad {
+                            break;
+                        }
+                    }
+                    Invalid => {
+                        return None;
+                    }
+                }
+            }
+
+            if self.total_added > 0 {
+                if (self.buf_ct != 0) && (!self.had_pad) {
+                    return Some(self.buf_left.clone());
+                }
+            }
+
+            None
+        }
+    }
+
+    impl Iterator for FromBase64Iter<'_> {
+        type Item = u8;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            self.next_byte()
+        }
+    }
+
+    pub trait CjFromBase64Iter {
+        fn iter_b64_to_byte(&self) -> FromBase64Iter;
+    }
+
+    impl CjFromBase64Iter for &str {
+        fn iter_b64_to_byte(&self) -> FromBase64Iter {
+            FromBase64Iter::new(self.chars())
+        }
+    }
+
+    /// converts a Base64 encoded str into a vec of bytes
+    /// ```
+    /// # use cj_common::prelude::base64_to_bytes;
+    /// let v = base64_to_bytes("TWFueSBoYW5kcyBtYWtlIGxpZ2h0IHdvcmsu");
+    /// assert!(v.is_some());
+    /// if let Some(x) = v {
+    ///    let s = "Many hands make light work.".as_bytes().to_vec();
+    ///    assert_eq!(x, s);
+    /// }
+    /// ```
+    pub fn base64_to_bytes(data: &str) -> Option<Vec<u8>> {
+        let mut v = Vec::<u8>::with_capacity((data.len() as f64 * 0.8) as usize);
+        let mut buf_left = 0u8;
+        let mut buf_ct = 0usize;
+        let mut had_pad = false;
+        //let mut had_error = false;
+        let mut total_added = 0usize;
+        for c in data.chars() {
+            let r = b64_char_to_ord(&c);
+            match r {
+                CharToOrdResult::Ok(b) => {
+                    if had_pad {
+                        break;
+                    }
+                    match buf_ct {
+                        0 => {
+                            // buf_left is empty
+                            // drop two bits from 'b' and take 6
+                            // 00XXXXXX
+                            buf_left = b << 2;
+                            buf_ct += 1;
+                        }
+                        1 => {
+                            // buf_left has 6 bits XXXXXX00
+                            // drop two bits from 'b' and take 2
+                            buf_left += (b << 2) >> 6;
+                            // push the complete byte
+                            v.push(buf_left);
+                            // now take the remaining 4
+                            buf_left = b << 4;
+                            buf_ct += 1;
+                            total_added += 1;
+                        }
+                        2 => {
+                            // buf_left has 4 bits XXXX0000
+                            // drop two bits from 'b' and take 4
+                            buf_left += (b << 2) >> 4;
+                            // push the complete byte
+                            v.push(buf_left);
+                            buf_left = b << 6;
+                            buf_ct += 1;
+                            total_added += 1;
+                        }
+                        3 => {
+                            // buf_left has 2 bits XX000000
+                            // 'b' only has 6 bits. take them all
+                            buf_left += b;
+                            // push the complete byte
+                            v.push(buf_left);
+                            buf_ct = 0;
+                            total_added += 1;
+                        }
+                        _ => unreachable!(),
+                    }
+                }
+                Pad => had_pad = true,
+                WhiteSpace => {
+                    if had_pad {
+                        break;
+                    }
+                }
+                Invalid => {
+                    //had_error = true;
+                    break;
+                }
+            }
+        }
+
+        if total_added > 0 {
+            if (buf_ct != 0) && (!had_pad) {
+                v.push(buf_left);
+            }
+            // we were able to decode something, even if there was an error. return what we have.
+            return Some(v);
+        }
+
+        None
     }
 
     #[cfg(test)]
@@ -370,6 +706,90 @@ pub mod b64 {
                 s2.push(c);
             }
             assert_eq!(s2.as_str(), "TWFueSBoYW5kcyBtYWtlIGxpZ2h0IHdvcmsu");
+        }
+
+        #[test]
+        fn test_from_b64_1() {
+            let v = base64_to_bytes("TWFueSBoYW5kcyBtYWtlIGxpZ2h0IHdvcmsu");
+            assert!(v.is_some());
+            if let Some(x) = v {
+                let s = "Many hands make light work.".as_bytes().to_vec();
+                assert_eq!(x, s);
+            }
+        }
+
+        #[test]
+        fn test_from_b64_2() {
+            let v = base64_to_bytes("TWFueSBoYW5kcyBtYWtlIGxpZ2h0IHdvcmsuLg==");
+            assert!(v.is_some());
+            if let Some(x) = v {
+                let s = "Many hands make light work..".as_bytes().to_vec();
+                assert_eq!(x, s);
+            }
+        }
+
+        #[test]
+        fn test_from_b64_3() {
+            let v = base64_to_bytes("TWFueSBoYW5kcyBtYWtlIGxpZ2h0IHdvcmsuLi4=");
+            assert!(v.is_some());
+            if let Some(x) = v {
+                let s = "Many hands make light work...".as_bytes().to_vec();
+                assert_eq!(x, s);
+            }
+        }
+
+        #[test]
+        fn test_from_b64_4() {
+            let v = base64_to_bytes("TWFueSBoYW5kcyBtYWtl   IGxpZ2h0IHdvcmsuLi4=");
+            assert!(v.is_some());
+            if let Some(x) = v {
+                let s = "Many hands make light work...".as_bytes().to_vec();
+                assert_eq!(x, s);
+            }
+        }
+
+        #[test]
+        fn test_from_b64_5() {
+            let v = base64_to_bytes("TWFueSBoYW5kcyBtYWtl   IGxpZ2h0IHdvcmsuLi4=nope");
+            assert!(v.is_some());
+            if let Some(x) = v {
+                let s = "Many hands make light work...".as_bytes().to_vec();
+                assert_eq!(x, s);
+            }
+        }
+
+        #[test]
+        fn test_from_b64_6() {
+            let v = base64_to_bytes("TWFueSBoYW5kcyBtYWtl   IGxpZ2h0IHdvcmsuLi4=&");
+            assert!(v.is_some());
+            if let Some(x) = v {
+                let s = "Many hands make light work...".as_bytes().to_vec();
+                assert_eq!(x, s);
+            }
+        }
+
+        #[test]
+        fn test_from_b64_iter_1() {
+            let mut v = Vec::new();
+            for b in "TWFueSBoYW5kcyBtYWtlIGxpZ2h0IHdvcmsu".iter_b64_to_byte() {
+                v.push(b);
+            }
+            assert!(v.len() > 0);
+            let r = String::from_utf8_lossy(v.as_slice());
+            let s = "Many hands make light work.";
+            assert_eq!(r.to_string().as_str(), s);
+        }
+
+        #[test]
+        fn test_from_b64_iter_2() {
+            let mut v = Vec::new();
+            for b in "TWFueSBoYW5kcyBtYWtl   IGxpZ2h0IHdvcmsuLi4=&".iter_b64_to_byte() {
+                v.push(b);
+            }
+            assert!(v.len() > 0);
+            let r = String::from_utf8_lossy(v.as_slice());
+            let s = "Many hands make light work...";
+            assert_eq!(r.to_string().as_str(), s);
         }
     }
 }
